2be987835bece995cc01e7fcf503790226092eb6
Kurapika <victorakhihiero@gmail.com>
2023-07-17 15:08:21 +0100

    last digit trial

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,17 +1,34 @@
+# import random
+# number = random.randint(-10000, 10000)
+
+# last_digit = abs(number) % 10
+# string = ""
+
+# if number < 0:
+#     last_digit = -last_digit
+
+# if last_digit > 5:
+#     string = "greater than 5"
+# elif last_digit == 0:
+#     string = "0"
+# else:
+#     string = "less than 6 and not 0"
+
+# print("Last digit of {} is {} and is {}".format(number, last_digit, string))
+
+
+#!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
-string = ""
 
-if number < 0:
-    last_digit = -last_digit
+print("Last digit of {}, is {} and is".format(number, last_digit), end=" ")
 
 if last_digit > 5:
-    string = "greater than 5"
+    print("greater than 5")
 elif last_digit == 0:
-    string = "0"
+    print("0")
 else:
-    string = "less than 6 and not 0"
-
-print("Last digit of {} is {} and is {}".format(number, last_digit, string))
+    print("less than 6 and not 0")
