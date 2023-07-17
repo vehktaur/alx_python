6ee33bd79d9891e06fae99f61a36362f51e116d7
Kurapika <victorakhihiero@gmail.com>
2023-07-17 14:42:02 +0100

    last digit update

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,10 +8,10 @@ if number < 0:
     last_digit = -last_digit
 
 if last_digit > 5:
-    string = "and is greater than 5"
+    string = "greater than 5"
 elif last_digit == 0:
-    string = "and is 0"
+    string = "0"
 else:
-    string = "and is less than 6 and not 0"
+    string = "less than 6 and not 0"
 
-print("Last digit of {} is {} {}".format(number, last_digit, string))
+print("Last digit of {} is {} and is {}".format(number, last_digit, string))
