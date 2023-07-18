f00001395ac0e325239616ca3f26dd565098fcb5
Kurapika <victorakhihiero@gmail.com>
2023-07-18 11:40:21 +0100

    update 5 and 6 task files

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,7 @@
-for x in range(100):
-    if x == 99:
-        print("{}".format(x))
+range_length = 200
+
+for x in range(range_length):
+    if x == len(range(range_length))-1:
+        print("{:02}".format(x))
     else:
         print("{:02},".format(x), end=" ")
