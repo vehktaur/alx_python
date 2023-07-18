efd07921bf547f50ddf9ce8902c28f464787608b
Kurapika <victorakhihiero@gmail.com>
2023-07-18 16:07:57 +0100

    update file 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,7 @@
+
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=", ")
-print()
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
