1853afb03b104dd33d1fe07310d950b2db1e54d0
Kurapika <victorakhihiero@gmail.com>
2023-07-19 10:55:50 +0100

    change 6 file

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        if i == 8 and j == 9:
+        if i == len(range(10))-2 and j > i:
             print("{}{}".format(i, j))
         else:
             print("{}{}".format(i, j), end=", ")
