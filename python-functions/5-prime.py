ce08bbe97f232433874b8d60f810bb336ebc897e
Kurapika <victorakhihiero@gmail.com>
2023-07-20 15:42:27 +0100

    check again

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,11 +1,11 @@
 def is_prime(number):
     test_prime = 0
 
-    for num in range(1, number+1):
+    for num in range(2, number+1):
         if number % num == 0:
             test_prime += 1
 
-    if number < 1 or type(number) is not int or test_prime != 2:
+    if number < 1 or type(number) is not int or test_prime > 1:
         return False
     else:
         return True
