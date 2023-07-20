168e67c8fedcd4e5fff07e44b6133722bcc43d14
Kurapika <victorakhihiero@gmail.com>
2023-07-20 15:41:10 +0100

    check 5

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -5,7 +5,7 @@ def is_prime(number):
         if number % num == 0:
             test_prime += 1
 
-    if number < 1 or type(number) is not int or test_prime == 2:
+    if number < 1 or type(number) is not int or test_prime != 2:
         return False
     else:
         return True
