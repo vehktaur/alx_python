11d16bb4b3692007cbdc8f70b0b96d52cb46fe3f
Kurapika <victorakhihiero@gmail.com>
2023-07-24 22:04:17 +0100

    2nd trial

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,16 @@
 # Importing the add function from add_0.py
 from add_0 import add
 
-# Define the variables a and b
-a = 1
-b = 2
+def main():
+    # Define the variables a and b
+    a = 1
+    b = 2
 
-# Call the add function and store the result in the variable result
-result = add(a, b)
+    # Call the add function and store the result in the variable result
+    result = add(a, b)
 
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, result))
+    # Print the result using string formatting
+    print("{} + {} = {}".format(a, b, result))
 
-if __name__ == "__0-add__":
-    0-add()
+if __name__ == "__main__":
+    main()
