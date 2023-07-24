af2b7f885266b962c4ebbaf174b6eba769ac1fb9
Kurapika <victorakhihiero@gmail.com>
2023-07-24 22:44:46 +0100

    add 1-file

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,16 @@
+
+
+if __name__ == "__main__":
+
+    def arguments(*args):
+        length = len(args)
+        letter = "" if length == 1 else "s"
+        symbol = "." if length == 0 else ":"
+        print("{} argument{}{}".format(length, letter, symbol))
+        if length > 0:
+            i = 1
+            for arg in args:
+                print("{}: {}".format(i, arg))
+                i += 1
+
+arguments(3)
