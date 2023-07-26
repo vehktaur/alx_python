d6b034f920759c00927610ea6cb4302235fc2162
Kurapika <victorakhihiero@gmail.com>
2023-07-26 10:36:39 +0100

    update

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,17 +1,19 @@
 #! /usr/bin/python3
 
+import sys
 
-def arguments(*args):
-    length = len(args)
+
+def arguments(parameter):
+    length = len(parameter) - 1
     letter = "" if length == 1 else "s"
     symbol = "." if length == 0 else ":"
     print("{} argument{}{}".format(length, letter, symbol))
+    i = 1
     if length > 0:
-        i = 1
-        for arg in args:
-            print("{}: {}".format(i, arg))
+        while i < len(parameter):
+            print("{}: {}".format(i, parameter[i]))
             i += 1
 
 
 if __name__ == "__main__":
-    arguments()
+    arguments(sys.argv)
