b21410396d6d2273aee097f789e46999882d1db5
Kurapika <victorakhihiero@gmail.com>
2023-07-26 12:00:02 +0100

    add matrix file

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for list in matrix:
+        for list_item in list:
+            print("{}".format(list_item), end=" ")
+        else:
+            print()
+
