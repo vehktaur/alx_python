6f34cd03890ddbea380912ca4b964a8478852c14
Kurapika <victorakhihiero@gmail.com>
2023-07-26 12:37:36 +0100

    update

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for inner_matrix in matrix:
+        new_inner_matrix = []
+        for inner_item in inner_matrix:
+            new_inner_matrix.append(inner_item ** 2)
+        new_matrix.append(new_inner_matrix)
+
+    return new_matrix
