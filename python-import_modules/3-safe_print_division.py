12b9ff4f8ec92dc39d4027110734db26bec3c9bf
Kurapika <victorakhihiero@gmail.com>
2023-07-26 11:21:53 +0100

    updatee

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,2 +1,14 @@
+#!/usr/bin/python3
+
 def safe_print_division(a, b):
-    
+    try:
+        result = float(a/b)
+        return result
+    except:
+        return "Types are not compatible"
+    finally:
+        return "Inside result: {}".format(result)
+
+
+if __name__ == "__main__":
+    safe_print_division()
