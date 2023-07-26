d8459414ba7cb208dc580a54fdf6fba39806bd4a
Kurapika <victorakhihiero@gmail.com>
2023-07-26 12:09:40 +0100

    updatee

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,8 +2,10 @@
 
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
+        i = 0
         for list_item in list:
-            print("{:d}".format(list_item), end=" ")
-        else:
-            print()
-
+            if i == len(list) - 1:
+                print("{:d}".format(list_item))
+            else:
+                print("{:d}".format(list_item), end=" ")
+            i += 1
