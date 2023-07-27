9c2ead72b1d78d4615c79232e7479f3a91a7ff2d
Kurapika <victorakhihiero@gmail.com>
2023-07-27 14:19:19 +0100

    update

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    def double(item):
-        return item * number
-    new_list = list(map(double, my_list))
+    new_list = list(map(lambda item: item * number, my_list))
     return new_list
