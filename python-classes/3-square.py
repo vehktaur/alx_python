946ea1be35bd86d2d095637093ff8181b1d326ca
Kurapika <victorakhihiero@gmail.com>
2023-07-31 16:01:22 +0100

    fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -18,7 +18,7 @@ class Square:
 
         @size.setter
         def size(self, size):
-            if type(value) is not int:
+            if type(size) is not int:
                 raise TypeError("size must be an integer")
             elif size < 0:
                 raise ValueError("size must be >= 0")
