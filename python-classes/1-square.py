21d8b4db70a47a067b37cbc728859fccedb20387
Kurapika <victorakhihiero@gmail.com>
2023-07-31 15:22:24 +0100

    update

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+"""
+Size validation for the Square class
+"""
+
+
+class Square:
+
+    """
+    Size validation for the Square class
+    """
+
+    def __init__(self, size=0):
+        if size is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
