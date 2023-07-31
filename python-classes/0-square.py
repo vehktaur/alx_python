61681ce7d1eba73f7b735eb59499d6a4bfb10ffc
Kurapika <victorakhihiero@gmail.com>
2023-07-31 13:26:11 +0100

    add fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,7 +4,7 @@ a simple class
 
 class Square:
     """
-    provide an interger for the size of the square
+    provide an integer for the size of the square
     """
     def __init__(self, size):
         self.__size = size
