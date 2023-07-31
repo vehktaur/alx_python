cd8901398b545608cd4f61bf23290520a52aa287
Kurapika <victorakhihiero@gmail.com>
2023-07-31 13:25:44 +0100

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,10 @@
+"""
+a simple class 
+"""
+
 class Square:
     """
     provide an interger for the size of the square
     """
     def __init__(self, size):
-        """
-        provide an interger for the size of the square
-        """
         self.__size = size
