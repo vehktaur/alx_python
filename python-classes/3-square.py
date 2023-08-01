d8bd47fcd9d311109458136cef50ca7f57ed93c9
Kurapika <victorakhihiero@gmail.com>
2023-08-01 13:37:01 +0100

    update

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,18 +12,20 @@ class Square:
     def __init__(self, size=0):
         self.size = size
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self, size):
-            if type(size) is not int or type(size) is str:
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         return self.size ** 2
+
+
