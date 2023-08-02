75b566a2c83da3ebad6f41fb2ac0b8c2426f10ad
Kurapika <victorakhihiero@gmail.com>
2023-08-02 22:56:31 +0100

    fixxes

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -9,8 +9,9 @@ def inherits_from(obj, a_class):
     is an instance of a descendant of the class, 
     either directly or indirectly
     """
-    obj_class = type(obj)
-    if issubclass(obj_class, a_class):
+
+    obj_class = obj.__class__
+    if issubclass(obj_class, a_class) and obj_class is not a_class:
         return True
     else:
         return False
