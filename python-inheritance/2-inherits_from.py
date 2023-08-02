3349de18cdce46ce275b0a9e0c2eaecaa4cba6f5
Kurapika <victorakhihiero@gmail.com>
2023-08-02 22:47:25 +0100

    updatee

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,16 @@
+"""
+third python inheritance task
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    this function checks if the object
+    is an instance of a descendant of the class, 
+    either directly or indirectly
+    """
+    obj_class = obj.__class__
+    if issubclass(obj_class, a_class) and obj_class is not a_class:
+        return True
+    else:
+        return False
