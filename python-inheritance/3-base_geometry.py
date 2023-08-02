341bd61b6903eb519abc420508c7ee395045c7b2
Kurapika <victorakhihiero@gmail.com>
2023-08-02 23:10:09 +0100

    updatee

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,10 @@
+"""
+fourth python inheritance task
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    empty class
+    """
+    pass
---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,11 @@
+"""
+fourth python inheritance task
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    non empty class
+    """
+    def area(self):
+        print("area() is not implemented")
