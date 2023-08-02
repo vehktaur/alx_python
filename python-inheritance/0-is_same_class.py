88ee24650fe4170022ee30a168c15cae0c89e83d
Kurapika <victorakhihiero@gmail.com>
2023-08-02 21:09:18 +0100

    checkkk

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -8,7 +8,7 @@ def is_same_class(obj, a_class):
     This checks if the class of the object 
     is the same as the class of the class
     """
-    if isinstance(obj, a_class) or obj.__class__ is not a_class.__class__:
+    if obj.__class__.__name__ == a_class.__name__:
         return True
     else:
         return False
