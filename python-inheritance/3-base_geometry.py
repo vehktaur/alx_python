8e460bc2acdd07723a536341b34c546e719a5a67
Kurapika <victorakhihiero@gmail.com>
2023-08-04 03:21:19 +0100

    fix

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,10 +3,16 @@ fourth python inheritance task
 """
 
 
-class BaseGeometry(object):
+class BaseGeometry():
     """
     created here is an empty class
     that does nothing
     """
+
     def __dir__(self):
-        return [attr for attr in super().__dir__() if not "__init_subclass__"]
+        variables = super().__dir__()
+        new_variables = []
+        for var in variables:
+            if var != "__init_subclass__":
+                new_variables.append(var)
+        return new_variables
