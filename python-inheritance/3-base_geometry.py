967c5fe327ba9a42be5c4e2ef2d18a361f9a953a
Kurapika <victorakhihiero@gmail.com>
2023-08-04 03:48:32 +0100

    latest

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,15 +6,19 @@ fourth python inheritance task
 class BaseGeometryMeta(type):
     def __dir__(self):
         variables = super().__dir__()
-        new_variables = [var for var in variables if var != "__init_subclass__"]
+        new_variables = [
+            var for var in variables if var != "__init_subclass__"]
         return new_variables
+
 
 class BaseGeometry(metaclass=BaseGeometryMeta):
     """
     created here is an empty class
     that does nothing
     """
+
     def __dir__(self):
         variables = super().__dir__()
-        new_variables = [var for var in variables if var != "__init_subclass__"]
+        new_variables = [
+            var for var in variables if var != "__init_subclass__"]
         return new_variables
--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,5 +1,5 @@
 """
-fifth python inheritance task
+sixth python inheritance task
 """
 
 
@@ -24,3 +24,10 @@ class BaseGeometry(metaclass=BaseGeometryMeta):
 
     def area(self):
         raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        else:
+            return f"{name} is {value}"
