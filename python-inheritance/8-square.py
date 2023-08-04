b8f0f3aab8bc768180a7746fdf4f1de0b36ea591
Kurapika <victorakhihiero@gmail.com>
2023-08-04 08:52:52 +0100

    add changes

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -55,6 +55,11 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
+    """
+    This class will print a square with dimensions
+    of the provided size
+    """
+
     def __init__(self, size):
         self.__size = super().integer_validator("size", size)
         super().__init__(size, size)
