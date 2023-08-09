3ac7bcccb47bc92fd999f26da3abed567f7ad42e
Kurapika <victorakhihiero@gmail.com>
2023-08-09 16:42:43 +0100

    update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,10 +9,17 @@ class Square(Rectangle):
     """
     square class which inherits from the rectangle class
     """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        square initialization method
+        """
         super().__init__(size, size, x, y, id)
         self.__height = size
         self.__width = size
 
     def __str__(self):
+        """
+        square custom string representation
+        """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
