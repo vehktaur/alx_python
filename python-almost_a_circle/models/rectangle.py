1bd86040c670fd276b5a97451209ca94cb70c1e0
Kurapika <victorakhihiero@gmail.com>
2023-08-10 01:58:42 +0100

    check

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,22 +3,7 @@ rectangle module for the rectangle class
 """
 
 
-class Base:
-    """
-    Base class for all other classes
-    """
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        initialization function with id attribute set
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
+from models.base import Base
 
 class Rectangle(Base):
     """
