de71c309463854812afabb7e7d0b615d5de51134
Kurapika <victorakhihiero@gmail.com>
2023-08-10 02:25:30 +0100

    check

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -2,56 +2,7 @@
 8th python inheritance task
 """
 
-
-class BaseGeometryMeta(type):
-    def __dir__(self):
-        variables = super().__dir__()
-        new_variables = [
-            var for var in variables if var != "__init_subclass__"]
-        return new_variables
-
-
-class BaseGeometry(metaclass=BaseGeometryMeta):
-    """
-    non empty class
-    """
-
-    def __dir__(self):
-        variables = super().__dir__()
-        new_variables = [
-            var for var in variables if var != "__init_subclass__"]
-        return new_variables
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        else:
-            return value
-
-
-class Rectangle(BaseGeometry):
-
-    """
-    This creates a rectangle object
-    """
-
-    def __init_subclass__(cls):
-        return object.__init_subclass__()
-
-    def __init__(self, width, height):
-        self.__width = super().integer_validator("width", width)
-        self.__height = super().integer_validator("height", height)
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        return self.__width * self.__height
+Rectangle = __import__('7-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -63,3 +14,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.__size = super().integer_validator("size", size)
         super().__init__(size, size)
+
+
+print(issubclass(Square, Rectangle))
