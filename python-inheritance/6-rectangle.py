10df1a8f76fa06237aee62dce414729f53f38f1d
Kurapika <victorakhihiero@gmail.com>
2023-08-10 02:31:37 +0100

    fix

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,36 +2,7 @@
 seventh python inheritance task
 """
 
-
-class BaseGeometryMeta(type):
-    def __dir__(self):
-        variables = super().__dir__()
-        new_variables = [
-            var for var in variables if var != "__init_subclass__"]
-        return new_variables
-
-
-class BaseGeometry(metaclass=BaseGeometryMeta):
-    """
-    non empty class
-    """
-
-    def __dir__(self):
-        variables = super().__dir__()
-        new_variables = [
-            var for var in variables if var != "__init_subclass__"]
-        return new_variables
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        else:
-            return value
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
