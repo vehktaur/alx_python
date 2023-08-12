100b2be0b75dcaab4740e30f83c9395be743cb1f
Kurapika <victorakhihiero@gmail.com>
2023-08-12 13:03:27 +0100

    update

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -11,18 +11,12 @@ password = sys.argv[2]
 session = requests.Session()
 session.auth = (username, password)
 
-try:
-    # Send a GET request to the GitHub API for user information
-    response = session.get("https://api.github.com/user")
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        user_info = response.json()
-        user_id = user_info["id"]
-        print(f"{user_id}")
-    else:
-        print(f"None")
-    
-except requests.RequestException as e:
-    print("An error occurred:", e)
-    sys.exit(1)
+
+# Send a GET request to the GitHub API for user information
+response = session.get("https://api.github.com/user")
+
+# Check if the request was successful
+user_info = response.json()
+user_id = user_info.get("id")
+print(user_id)
+
