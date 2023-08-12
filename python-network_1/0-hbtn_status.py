e21fba0651a532d89c24e811e8f60f5d24ae23a1
Kurapika <victorakhihiero@gmail.com>
2023-08-12 08:44:05 +0100

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,11 @@
 """
-import task 0
+task 0 module
 """
 import requests
 
 
 req = requests.get("https://alu-intranet.hbtn.io/status")
+
+print("Body response:")
+print("\t- type:", type(req.text))
+print("\t- content:", req.text)
