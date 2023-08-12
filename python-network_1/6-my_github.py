5e92b438b107a8c598c4f4ce564f6535d1c2c24a
Kurapika <victorakhihiero@gmail.com>
2023-08-12 13:04:18 +0100

    update

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -8,12 +8,9 @@ import sys
 username = sys.argv[1]
 password = sys.argv[2]
 
-session = requests.Session()
-session.auth = (username, password)
-
 
 # Send a GET request to the GitHub API for user information
-response = session.get("https://api.github.com/user")
+response = requests.get("https://api.github.com/user", auth= (username, password))
 
 # Check if the request was successful
 user_info = response.json()
