34701ae3855aa613d47f8df34c2eee589ee91c9f
Kurapika <victorakhihiero@gmail.com>
2023-08-12 11:09:22 +0100

    update

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,31 @@
+"""
+module documentation
+"""
+
+import requests
+import sys
+
+q = ""
+
+if len(sys.argv) > 1:
+    arg = sys.argv[1]
+    if arg is str:
+        q = arg
+        
+        payload = {"q" : q}
+        
+        r = requests.post("http://0.0.0.0:5000/search_user", data = payload)
+        
+        text = r.json()
+        
+        if text:
+            id = text.get("id")
+            name = text.get("name")
+            if id is not None and name is not None:
+                print("[{}] {}".format(id, name))
+            else:
+                print("Not a valid JSON")
+        else:
+            print("No result") 
+        
+
