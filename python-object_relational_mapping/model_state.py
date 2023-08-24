3a54786b0a2331759658ef000d1557da56fc177f
Kurapika <victorakhihiero@gmail.com>
2023-08-24 22:11:34 +0100

    check

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,9 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class State(Base):
+    """
+    class documentation
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
