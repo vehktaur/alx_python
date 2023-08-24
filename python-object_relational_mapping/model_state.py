d091d7fe5a209c37961829dbb30e96700197e2f8
Kurapika <victorakhihiero@gmail.com>
2023-08-24 22:30:54 +0100

    check

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -18,11 +18,11 @@ Base = declarative_base()
 
 class State(Base):
     """
-        Represents a state entity in the MySQL table 'states'.
+    Represents a state entity in the MySQL table 'states'.
     
-        Attributes:
-        id (int): An auto-generated, unique integer representing the primary key.
-        name (str): A string with a maximum of 128 characters representing the state name.
+    Attributes:
+    id (int): An auto-generated, unique integer representing the primary key.
+    name (str): A string with a maximum of 128 characters representing the state name.
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
