ed78ea1e26df9c956ec0b058f7752be1e837349f
Kurapika <victorakhihiero@gmail.com>
2023-08-24 22:10:55 +0100

    check

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,27 @@
+"""
+model state documentation
+"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Define the State class
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+# Connect to the MySQL server
+engine = create_engine('mysql://username:password@localhost:3306/database_name')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Import the State class
+from model_state import State
+
+# Create the table
+Base.metadata.create_all(engine)
+
+
