b1c73788831fbbe1c01cdb73b92f6a8eb0c453de
Kurapika <victorakhihiero@gmail.com>
2023-08-24 03:58:24 +0100

    check

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,8 +4,8 @@ first task in Python ORM
 
 import MySQLdb
 
-db = MySQLdb.connect(host = "localhost", user = "watashi", passwd =  "passwd")
+db = MySQLdb.connect(host = "localhost", user = "watashi", passwd =  "passwd", database = "hbtn_0e_0_usa")
 
 cur = db.cursor()
 
-cur.execute("USE hbtn_0e_0_usa; SELECT * FROM states ORDER_BY id;" )
+cur.execute("SELECT * FROM states ORDER_BY id" )
