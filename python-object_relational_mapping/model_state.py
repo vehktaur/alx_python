e77c9b77ebc4a6d993f0aa4fc80d1df6a286bfc4
Kurapika <victorakhihiero@gmail.com>
2023-08-24 22:14:31 +0100

    check

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,20 @@
 """
 model state documentation
 """
+from model_state import State
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+user = argv[1]
+passwd = argv[2]
+database = argv[3]
 
 # Define the State class
 Base = declarative_base()
+
 
 class State(Base):
     """
@@ -16,15 +24,14 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 # Connect to the MySQL server
-engine = create_engine('mysql://username:password@localhost:3306/database_name')
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, passwd, database))
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # Import the State class
-from model_state import State
 
 # Create the table
 Base.metadata.create_all(engine)
-
-
