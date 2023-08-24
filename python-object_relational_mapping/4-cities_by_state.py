e33253c29a3dca1a76f86f5d606ca8debf23de77
Kurapika <victorakhihiero@gmail.com>
2023-08-24 14:15:07 +0100

    fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -19,8 +19,8 @@ def main():
     the main function
     """
     query = """
-    SELECT cities.id, cities.name, states.name 
-    FROM cities 
+    SELECT cities.id, cities.name, states.name
+    FROM cities
     JOIN states ON states.id = cities.state_id ORDER BY cities.id;
     """
     cur.execute(query)
