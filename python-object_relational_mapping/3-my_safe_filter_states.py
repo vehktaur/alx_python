65b449a6b4d28c3eb0adc1feacc0a9e16c655744
Kurapika <victorakhihiero@gmail.com>
2023-08-24 13:44:06 +0100

    check

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+"""
+first task in Python ORM
+"""
+
+import MySQLdb
+import sys
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+database = sys.argv[3]
+arg = sys.argv[4]
+
+db = MySQLdb.connect(user=user, passwd=passwd, database=database)
+
+cur = db.cursor()
+
+
+def main():
+    """
+    the main function
+    """
+    query = "SELECT * FROM states WHERE BINARY name='{}' ORDER BY id;".format(
+        arg)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
