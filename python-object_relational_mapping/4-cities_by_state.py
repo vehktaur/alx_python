045b849f875c09c69c4d2acdde43a2da154e46d2
Kurapika <victorakhihiero@gmail.com>
2023-08-24 14:08:32 +0100

    check

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+"""
+first task in Python ORM
+"""
+
+import MySQLdb
+import sys
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+database = sys.argv[3]
+
+db = MySQLdb.connect(user=user, passwd=passwd, database=database)
+
+cur = db.cursor()
+
+
+def main():
+    """
+    the main function
+    """
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id;"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
