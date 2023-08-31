5639acff944ed9822554b30bf015f050ac6fc010
Kurapika <victorakhihiero@gmail.com>
2023-08-31 05:46:29 +0100

    check

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,36 @@
+"""
+module documentation for task 2 file
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    mandatory docs
+    """
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    mandatory docs
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """
+    mandatory docs
+    """
+    return f"C {escape(text)}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
