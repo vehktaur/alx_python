2a91dafa967dfbb24a792f26b0bd1aafe7d5efe4
Kurapika <victorakhihiero@gmail.com>
2023-08-31 05:55:29 +0100

    try

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,44 @@
+"""
+mandatory module documentation
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    mandatory function docs
+    """
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    mandatory function docs
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """
+    mandatory function docs
+    """
+    return f"C {escape(text)}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """
+    mandatory function docs
+    """
+    return f"Python {escape(text)}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
