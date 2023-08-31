07c78448164ee9e4d2b3b5cf779d44548a063a1c
Kurapika <victorakhihiero@gmail.com>
2023-08-31 05:37:08 +0100

    try

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,27 @@
+"""
+module documentation for task 1 file
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    mandatory documentation
+    """
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    mandatory docs
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
