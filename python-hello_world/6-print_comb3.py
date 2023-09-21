3ed7c23e865feebc850d3e4050bd31ca52905945
vehktaur <victorakhihiero@gmail.com>
2023-09-21 13:10:26 +0100

    undid that

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -3,6 +3,5 @@ for i in range(10):
     for j in range(i + 1, 10):
         if i == len(range(10))-2 and j > i:
             print("{}{}".format(i, j))
-
         else:
             print("{}{}".format(i, j), end=", ")
