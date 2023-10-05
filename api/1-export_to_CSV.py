dd95a18cfebc0f1e3bbe87248aadb7bb983dada6
vehktaur <victorakhihiero@gmail.com>
2023-10-05 05:50:17 +0100

    check

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,7 +2,6 @@ import csv
 import requests
 import sys
 
-
 def get_employee_info(employee_id):
     # URL for employee details
     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
@@ -13,7 +12,7 @@ def get_employee_info(employee_id):
         # Fetch employee details
         employee_response = requests.get(employee_url)
         employee_data = employee_response.json()
-
+        
         # Fetch TODO list
         todo_response = requests.get(todo_url)
         todo_data = todo_response.json()
@@ -23,8 +22,7 @@ def get_employee_info(employee_id):
         completed_tasks = sum(1 for task in todo_data if task.get("completed"))
 
         # Print employee's TODO list progress
-        print(
-            f"Employee {employee_data.get('name')} is done with tasks ({completed_tasks}/{total_tasks}):")
+        print(f"Employee {employee_data.get('name')} is done with tasks ({completed_tasks}/{total_tasks}):")
 
         # Print titles of completed tasks
         for task in todo_data:
@@ -34,23 +32,22 @@ def get_employee_info(employee_id):
         # Export data to CSV
         csv_file_name = f"{employee_id}.csv"
         with open(csv_file_name, mode='w', newline='') as csv_file:
-            csv_writer = csv.writer(
-                csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            
             # Write TODO list data to CSV
             for task in todo_data:
-                csv_writer.writerow([employee_id, employee_data.get(
-                    'name'), task.get('completed'), task.get('title')])
-
+                csv_writer.writerow([employee_id, employee_data.get('name'), str(task.get('completed')), task.get('title')])
+        
+        print(f"Data exported to {csv_file_name}")
+        
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         sys.exit(1)
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
-
+    
     employee_id = int(sys.argv[1])
     get_employee_info(employee_id)
