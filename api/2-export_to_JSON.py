8bdaf8ee8a205c718d65403c94a4966406eb1555
vehktaur <victorakhihiero@gmail.com>
2023-10-07 17:24:02 +0100

    check

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,11 +1,11 @@
 """
-This module serves as a utility for interacting with a REST API to retrieve employee data and their associated TODO tasks. It provides functionality to request data from the API, process it, and export it into JSON format.
+This module serves as a utility for interacting with a REST API to retrieve employee data and their associated todo tasks. It provides functionality to request data from the API, process it, and export it into JSON format.
 
 Usage:
-    To use this module, provide an employee ID as a command-line argument when running the script. The module will then retrieve information about the specified employee, including their name and a list of TODO tasks. It calculates the progress of completed tasks and exports this data into a JSON file.
+    To use this module, provide an employee ID as a command-line argument when running the script. The module will then retrieve information about the specified employee, including their name and a list of todo tasks. It calculates the progress of completed tasks and exports this data into a JSON file.
     
 Module Structure:
-    - The module first constructs the URLs for employee details and their TODO list based on the provided employee ID.
+    - The module first constructs the URLs for employee details and their todo list based on the provided employee ID.
     - It fetches data from the API using the `requests` library.
     - The module calculates the total number of tasks and the number of completed tasks to determine progress.
     - It prints the progress information and the titles of completed tasks to the console.
@@ -20,33 +20,33 @@ import sys
 
 employee_id = sys.argv[1]
 
-# URL for employee details
+
 employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-# URL for employee's TODO list
+
 todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-# Fetch employee details
+
 employee_response = requests.get(employee_url)
 employee_data = employee_response.json()
 
-# Fetch TODO list
+
 todo_response = requests.get(todo_url)
 todo_data = todo_response.json()
 
-# Calculate TODO list progress
+
 total_tasks = len(todo_data)
 completed_tasks = sum(1 for task in todo_data if task.get("completed"))
 
-# Print employee's TODO list progress
+
 print(
     f"Employee {employee_data.get('name')} is done with tasks ({completed_tasks}/{total_tasks}):")
 
-# Print titles of completed tasks
+
 for task in todo_data:
     if task.get("completed"):
         print(f"\t{task['title']}")
 
-# Create a dictionary to represent the data
+
 employee_json_data = {
     str(employee_id): [
         {
@@ -58,7 +58,7 @@ employee_json_data = {
     ]
 }
 
-# Export data to JSON
+
 json_file_name = f"{employee_id}.json"
 with open(json_file_name, 'w') as json_file:
     json.dump(employee_json_data, json_file, indent=4)
