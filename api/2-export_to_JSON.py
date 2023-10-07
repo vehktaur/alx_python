9a115e981d46f391bb4eecb72201222ae273ef88
vehktaur <victorakhihiero@gmail.com>
2023-10-07 16:52:11 +0100

    check

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,62 +6,54 @@ import requests
 import sys
 
 
-def get_employee_info(employee_id):
-    # URL for employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    # URL for employee's TODO list
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+employee_id = sys.argv[1]
 
-    try:
-        # Fetch employee details
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
+# URL for employee details
+employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+# URL for employee's TODO list
+todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-        # Fetch TODO list
-        todo_response = requests.get(todo_url)
-        todo_data = todo_response.json()
+try:
+    # Fetch employee details
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
 
-        # Calculate TODO list progress
-        total_tasks = len(todo_data)
-        completed_tasks = sum(1 for task in todo_data if task.get("completed"))
+    # Fetch TODO list
+    todo_response = requests.get(todo_url)
+    todo_data = todo_response.json()
 
-        # Print employee's TODO list progress
-        print(
-            f"Employee {employee_data.get('name')} is done with tasks ({completed_tasks}/{total_tasks}):")
+    # Calculate TODO list progress
+    total_tasks = len(todo_data)
+    completed_tasks = sum(1 for task in todo_data if task.get("completed"))
 
-        # Print titles of completed tasks
-        for task in todo_data:
-            if task.get("completed"):
-                print(f"\t{task['title']}")
+    # Print employee's TODO list progress
+    print(
+        f"Employee {employee_data.get('name')} is done with tasks ({completed_tasks}/{total_tasks}):")
 
-        # Create a dictionary to represent the data
-        employee_json_data = {
-            "USER_ID": [
-                {
-                    "task": task.get("title"),
-                    "completed": task.get("completed"),
-                    "username": employee_data.get('name')
-                }
-                for task in todo_data
-            ]
-        }
+    # Print titles of completed tasks
+    for task in todo_data:
+        if task.get("completed"):
+            print(f"\t{task['title']}")
 
-        # Export data to JSON
-        json_file_name = f"{employee_id}.json"
-        with open(json_file_name, mode='w') as json_file:
-            json.dump(employee_json_data, json_file, indent=4)
+    # Create a dictionary to represent the data
+    employee_json_data = {
+        str(employee_id): [
+            {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": employee_data.get('username')
+            }
+            for task in todo_data
+        ]
+    }
 
-        print(f"Data exported to {json_file_name}")
+    # Export data to JSON
+    json_file_name = f"{employee_id}.json"
+    with open(json_file_name, 'w') as json_file:
+        json.dump(employee_json_data, json_file, indent=4)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+    print(f"Data exported to {json_file_name}")
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
+    sys.exit(1)
